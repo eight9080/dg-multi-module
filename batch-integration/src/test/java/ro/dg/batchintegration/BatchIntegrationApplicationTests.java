3c75f1b547f9a9af28210751acde21cc84092d63
eight9080 <eight9080@gmail.com>
2018-06-25 23:12:11 +0300

    batch integration

---
+++ batch-integration/src/test/java/ro/dg/batchintegration/BatchIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.dg.batchintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
